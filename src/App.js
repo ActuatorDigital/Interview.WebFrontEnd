4816d64f75ad3fc1617586473efb24a48739e012
Sebastiaan Fehr <seb@animaginedreality.com>
2021-10-26 21:26:33 +1000

    Added basic create-react-app solution and instructions.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="instruction-frame">
+      <div>
+        <h2 id="1-react-basics">1 - React Basics</h2>
+        <pre>
+          <code class="lang-bash">git checkout task/react-counter</code>
+        </pre>
+        <p>For this first task, don&#39;t worry about styling. The uglier the better. All we want to see in this step is that you can introduce a button to a page, which when clicked increments a counter which is rendered in a page.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
